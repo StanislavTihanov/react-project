2b85eadba48fec77b0da231a01db696720be7d4b
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2024-02-29 15:31:37 +0300

    END

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -1,44 +1,67 @@
 import { useState } from "react";
 import Button from "./Button/Button";
 
+function StateVsRef () {
+  const [value, setValue] = useState ('')
+  return(
+    <div>
+      <h3>Input value: {value}</h3>
+      <input type="text"  className="control"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  )
+}
+
 export default function FeedbackSection() {
-  const [name, setName] = useState("");
-  const [hasError, setHasError] = useState(false);
-  const [reason, setReason] = useState("help");
+  const [form, setForm] = useState({
+    name: '',
+    hasError: false,
+    reason: 'help'
+  })
 
   function handleNameChange(event) {
-    setName(event.target.value);
-    setHasError(event.target.value.trim().length === 0)
+    setForm((prev) => ({
+      ...prev,
+      name: event.target.value,
+      hasError: event.target.value.trim().length === 0,
+    }))
   }
 
   return (
     <section>
       <h3>Обратная связь</h3>
 
-      <form className="form">
+      <form className="form" style={{ marginBottom: '1rem'}}>
         <label htmlFor="name">Ваше имя</label>
         <input
           type="text"
           id="name"
           className="control"
-          value={name}
+          value={form.name}
+          style={{
+            border: form.hasError ? '1px solid red' : null,
+            }}
           onChange={handleNameChange}
-          style={{border: hasError ? '1px solid red' : null}}
         />
         <label htmlFor="reason">Причина обращения</label>
         <select
           id="reason"
           className="control"
-          value={reason}
-          onChange={(event) => setReason(event.target.value)}
+          value={form.reason}
+          onChange={(event) => 
+               setForm(prev => ({...prev, reason: event.target.value}))
+          }
         >
           <option value="error">Ошибка</option>
           <option value="help">Предложение</option>
           <option value="suggest">Нужна помощь</option>
         </select>
 
-        <Button>Отправить</Button>
+        <Button disabled={form.hasError} isActive={!form.hasError}>Отправить</Button>
       </form>
+      <StateVsRef/>
     </section>
   );
 }
